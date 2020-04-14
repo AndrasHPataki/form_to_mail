d19b75221cf51b068284530cb2ca7b6acf622cb7
AndrasHPataki <33844385+AndrasHPataki@users.noreply.github.com>
2020-04-14 15:25:38 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'form_to_mail',         
+  packages = ['form_to_mail'],   
+  version = '0.001',      
+  license='MIT',       
+  description = 'Simple configuration system for mail, required for forms packages', 
+  long_description='This library is recommended to integrate with the forms package. It works to simplify sending emails.',
+  author = 'AndrásPataki',                   
+  author_email = 'andras.h.pataki@gmail.com',     
+  url = 'https://github.com/AndrasHPataki/form_to_mail',  
+  download_url = 'https://github.com/AndrasHPataki/form_to_mail/archive/0.001.tar.gz',   
+  keywords = ['Mail', 'Delivery', 'Form','Flask'],  
+  install_requires=[            
+          'Flask-Mail','flask-wtf'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
